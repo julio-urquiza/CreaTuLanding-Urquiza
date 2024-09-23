3b4c1e64a4816b0f1de31646df477eeb944e1e9f
Julio_Urquiza <julio.cesar.urquiza4@gmail.com>
2024-09-23 19:39:11 -0300

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NavBar from './Components/NavBar/Navbar'
+import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
+
+function App() {
+  return (
+    <div className='App'>
+      <NavBar />
+      <ItemListContainer greeting={'Bienvenidos'}/>
+    </div>
+  )
+}
+
+export default App
